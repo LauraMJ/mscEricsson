a88530b5db2694f1785431478495ab165656ce64
LauraMJ <lauramjphelan@gmail.com>
2015-02-07 18:56:18 +0000

    Fixed column annotation and varchar lengths

--- project/src/project/Country.java
+++ project/src/project/Country.java
@@ -1,12 +1,15 @@
 package project;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class Country {
 	@Id
+	@Column(name="mcc")
 	private int mcc; //PK
+	@Column(name="country", length=45)
 	private String country;
 	
 	public Country() {
--- project/src/project/MCC_MNC.java
+++ project/src/project/MCC_MNC.java
@@ -1,12 +1,15 @@
 package project;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class MCC_MNC {
 	@Id
+	@Column(name="mcc_mnc")
 	private MCC_MNCCK mccAndMnc; //Composite key of MCC and MNC
+	@Column(name="operator", length=100)
 	private String operator;
 
 	public MCC_MNC() {

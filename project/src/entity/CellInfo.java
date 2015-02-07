955c16e2e80a76dc614656c34f1112f0963b17ca
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-02-07 17:52:36 +0000

    storage: add the CellInfo entity

---
+++ project/src/entity/CellInfo.java
@@ -0,0 +1,76 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CellInfo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cellInfoId;
+
+	private int cellId;
+	private int neVersion;
+	private String hier3Id, hier32Id, hier321Id;
+
+	public CellInfo() {
+	}
+
+	public CellInfo(int cellId, int neVersion, String hier3Id, String hier32Id, String hier321Id) {
+		this.cellId = cellId;
+		this.neVersion = neVersion;
+		this.hier3Id = hier3Id;
+		this.hier32Id = hier32Id;
+		this.hier321Id = hier321Id;
+	}
+
+	public int getCellInfoId() {
+		return cellInfoId;
+	}
+
+	public void setCellInfoId(int cellInfoId) {
+		this.cellInfoId = cellInfoId;
+	}
+
+	public int getCellId() {
+		return cellId;
+	}
+
+	public void setCellId(int cellId) {
+		this.cellId = cellId;
+	}
+
+	public int getNeVersion() {
+		return neVersion;
+	}
+
+	public void setNeVersion(int neVersion) {
+		this.neVersion = neVersion;
+	}
+
+	public String getHier3Id() {
+		return hier3Id;
+	}
+
+	public void setHier3Id(String hier3Id) {
+		this.hier3Id = hier3Id;
+	}
+
+	public String getHier32Id() {
+		return hier32Id;
+	}
+
+	public void setHier32Id(String hier32Id) {
+		this.hier32Id = hier32Id;
+	}
+
+	public String getHier321Id() {
+		return hier321Id;
+	}
+
+	public void setHier321Id(String hier321Id) {
+		this.hier321Id = hier321Id;
+	}
+}

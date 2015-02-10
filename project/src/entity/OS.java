763cc7fba9af7fe4d54882b9c02edc434c72a0c7
SiobhanKavanagh <siobhan_kavanagh04@hotmail.com>
2015-02-10 13:19:46 +0000

    got read of unused imports

--- project/src/entity/OS.java
+++ project/src/entity/OS.java
@@ -2,8 +2,6 @@ package entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name="OS")

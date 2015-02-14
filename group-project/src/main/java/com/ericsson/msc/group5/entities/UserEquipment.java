1ec605750e8964d0d22e6041f9b28f89a9fbf791
Harry McLaughlin <hamc17@hotmail.com>
2015-02-14 14:48:09 +0000

    Changed Maven compilation target version, removed UserEntity, added Apache POI dependency, added web.xml to comile correctly as WAR

--- group-project/src/main/java/com/ericsson/msc/group5/entities/UserEquipment.java
+++ group-project/src/main/java/com/ericsson/msc/group5/entities/UserEquipment.java
@@ -6,31 +6,32 @@ import javax.persistence.Id;
 
 @Entity
 public class UserEquipment {
-	
+
 	@Id
-	@Column(name="tac")
+	@Column(name = "tac")
 	private Integer TAC;
-	@Column(name="marketing_name", length=60)
+	@Column(name = "marketing_name", length = 60)
 	private String marketingName;
-	@Column(name="manufacturer", length=60)
+	@Column(name = "manufacturer", length = 60)
 	private String manufacturer;
-	@Column(name="access_capability")
-	private Integer accessCapability;
-	@Column(name="model", length=45)
+	@Column(name = "access_capability", length = 100)
+	private String accessCapability;
+	@Column(name = "model", length = 45)
 	private String model;
-	@Column(name="ue_type")
+	@Column(name = "ue_type")
 	private Integer UEType;
-	@Column(name="os")
+	@Column(name = "os")
 	private Integer OS;
-	@Column(name="input_mode")
+	@Column(name = "input_mode")
 	private Integer inputMode;
-	
-	public UserEquipment(){
-		
+
+	public UserEquipment() {
+
 	}
-	
-	public UserEquipment(Integer TAC, String marketingName, Integer accessCapability, 
-			String model, Integer UEType, Integer OS, Integer inputMode){
+
+	public UserEquipment(Integer TAC, String marketingName,
+			String accessCapability, String model, Integer UEType, Integer OS,
+			Integer inputMode) {
 		this.TAC = TAC;
 		this.marketingName = marketingName;
 		this.accessCapability = accessCapability;
@@ -39,19 +40,23 @@ public class UserEquipment {
 		this.OS = OS;
 		this.inputMode = inputMode;
 	}
-	
+
 	public Integer getTAC() {
 		return TAC;
 	}
+
 	public void setTAC(Integer tAC) {
 		TAC = tAC;
 	}
+
 	public String getMarketingName() {
 		return marketingName;
 	}
+
 	public void setMarketingName(String marketingName) {
 		this.marketingName = marketingName;
 	}
+
 	public String getManufacturer() {
 		return manufacturer;
 	}
@@ -59,33 +64,43 @@ public class UserEquipment {
 	public void setManufacturer(String manufacturer) {
 		this.manufacturer = manufacturer;
 	}
-	public Integer getAccessCapability() {
+
+	public String getAccessCapability() {
 		return accessCapability;
 	}
-	public void setAccessCapability(Integer accessCapability) {
+
+	public void setAccessCapability(String accessCapability) {
 		this.accessCapability = accessCapability;
 	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String model) {
 		this.model = model;
 	}
+
 	public Integer getUEType() {
 		return UEType;
 	}
+
 	public void setUEType(Integer uEType) {
 		UEType = uEType;
 	}
+
 	public Integer getOS() {
 		return OS;
 	}
+
 	public void setOS(Integer oS) {
 		OS = oS;
 	}
+
 	public Integer getInputMode() {
 		return inputMode;
 	}
+
 	public void setInputMode(Integer inputMode) {
 		this.inputMode = inputMode;
 	}

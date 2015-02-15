5ea6d2079888ffc48e7c7a46445dcbe7dbf82a56
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-02-15 12:46:28 +0000

    FailureClass: added Arquillian test.
    an arquillian test for the FailureClass entity

---
+++ group-project/src/test/java/com/ericsson/msc/group5/entities/FailureClassTest.java
@@ -0,0 +1,81 @@
+package com.ericsson.msc.group5.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class FailureClassTest {
+	@Deployment
+	public static Archive<?> createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(FailureClass.class.getPackage())
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Inject
+	private UserTransaction utx;
+
+	private static String INITIAL_DESCRIPTION = "HIGH PRIORITY ACCESS";
+	private static String UPDATED_DESCRIPTION = "EMERGENCY";
+
+	@Before
+	public void preparePersistenceTest() throws Exception {
+		clearData();
+		startTransaction();
+	}
+
+	@After
+	public void commitTransaction() throws Exception {
+		utx.commit();
+	}
+
+	@Test
+	public void failureClassTest() throws Exception {
+		int newId = 0;
+				
+		FailureClass createdFC = new FailureClass(newId, INITIAL_DESCRIPTION);
+		em.persist(createdFC);
+
+		FailureClass loadedFC = em.find(FailureClass.class, newId);
+		assertEquals("Failed to insert", INITIAL_DESCRIPTION, loadedFC.getDescription());
+
+		loadedFC.setDescription(UPDATED_DESCRIPTION);
+		FailureClass updatedFC = em.find(FailureClass.class, newId);
+		assertEquals("Failed to update", UPDATED_DESCRIPTION, updatedFC.getDescription());
+
+		em.remove(updatedFC);
+		FailureClass shouldBeNull = em.find(FailureClass.class, newId);
+		assertNull("Failed to delete", shouldBeNull);
+	}
+
+	private void clearData() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+		System.out.println("Dumping old records...");
+		em.createQuery("delete from com.ericsson.msc.group5.entities.FailureClass").executeUpdate();
+		utx.commit();
+	}
+
+	private void startTransaction() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+	}
+}

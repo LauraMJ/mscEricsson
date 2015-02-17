93f4fff19fc8c4c9228c99cec9a6036783611313
SiobhanKavanagh <siobhan_kavanagh04@hotmail.com>
2015-02-17 16:45:20 +0000

    Added some tests for lookup tables

--- group-project/src/main/java/com/ericsson/msc/group5/dao/jpa/PersistenceTest.java
+++ group-project/src/main/java/com/ericsson/msc/group5/dao/jpa/PersistenceTest.java
@@ -5,38 +5,52 @@ import com.ericsson.msc.group5.entities.Country;
 import com.ericsson.msc.group5.entities.EventCause;
 import com.ericsson.msc.group5.entities.EventCauseCK;
 import com.ericsson.msc.group5.entities.FailureClass;
+import com.ericsson.msc.group5.entities.InputMode;
 import com.ericsson.msc.group5.entities.MCC_MNC;
 import com.ericsson.msc.group5.entities.MCC_MNCCK;
+import com.ericsson.msc.group5.entities.OS;
+import com.ericsson.msc.group5.entities.UEType;
 import com.ericsson.msc.group5.entities.UserEquipment;
 
 public class PersistenceTest {
-	//Variables for laura's tests
+
+	// Variables for laura's tests
 	EventCauseCK eventCauseCK = new EventCauseCK(4102, 203);
 	MCC_MNCCK mccmnc = new MCC_MNCCK(123, 456);
-	
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		new PersistenceTest();
 	}
 
 	public PersistenceTest() {
 		PersistenceUtil.persist(new AccessCapability("TEST"));
 		System.out.println("Entity saved");
-		//PersistenceUtil.persist(new CellInfo(25, 12, "213421", "2341325", "TEST"));
-		System.out.println("Entity saved");
 		PersistenceUtil.persist(new FailureClass(4, "hello world"));
 		System.out.println("Entity saved");
-		
-		
-		//laura's tests
+
+		// Siobhan's tests
+		// PersistenceUtil.persist(new FailureTrace());
+		// System.out.println("Entity saved");
+		// PersistenceUtil.persist(new HierInfo());
+		// System.out.println("Entity saved");
+		PersistenceUtil.persist(new InputMode(1, "BASIC"));
+		System.out.println("Entity saved");
+		PersistenceUtil.persist(new OS(2, "BLACKBERRY"));
+		System.out.println("Entity saved");
+		PersistenceUtil.persist(new UEType(3, "HANDHELD"));
+		System.out.println("Entity saved");
+
+		// laura's tests
 		PersistenceUtil.persist(new Country(340, "Denmark"));
 		System.out.println("Entity saved");
-		PersistenceUtil.persist(new EventCause(eventCauseCK, "this is a description of the event"));
+		PersistenceUtil.persist(new EventCause(eventCauseCK,
+				"this is a description of the event"));
 		System.out.println("Entity saved");
 		PersistenceUtil.persist(new MCC_MNC(mccmnc, "some operator name"));
 		System.out.println("Entity saved");
-		PersistenceUtil.persist(new UserEquipment(123245, "TestMarketingName", "GSM 1800", "BrickPhone1.0", null, null, null));
+		PersistenceUtil.persist(new UserEquipment(123245, "TestMarketingName",
+				"GSM 1800", "BrickPhone1.0", null, null, null));
 		System.out.println("Entity saved");
-		
+
 	}
 }

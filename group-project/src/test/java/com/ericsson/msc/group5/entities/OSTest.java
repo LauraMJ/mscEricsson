d472e159daf0b041c3ac68ca641419753f65edae
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-02-18 12:11:59 +0000

    os: add arquillian tests

---
+++ group-project/src/test/java/com/ericsson/msc/group5/entities/OSTest.java
@@ -0,0 +1,79 @@
+package com.ericsson.msc.group5.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class OSTest {
+
+	@Deployment
+	public static Archive <?> createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(OS.class.getPackage())
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Inject
+	private UserTransaction utx;
+
+	private static String INITIAL_OS = "BLACKBERRY";
+	private static String UPDATED_OS = "IOS";
+
+	@Before
+	public void preparePersistenceTest() throws Exception {
+		clearData();
+		startTransaction();
+	}
+
+	@After
+	public void commitTransaction() throws Exception {
+		utx.commit();
+	}
+
+	@Test
+	public void failureClassTest() throws Exception {
+		int newId = 1;
+		OS createdOS = new OS(newId, INITIAL_OS);
+		em.persist(createdOS);
+
+		OS loadedOS = em.find(OS.class, newId);
+		assertEquals("Failed to insert", INITIAL_OS, loadedOS.getOs());
+
+		loadedOS.setOs(UPDATED_OS);
+		OS updatedOS = em.find(OS.class, newId);
+		assertEquals("Failed to update", UPDATED_OS, updatedOS.getOs());
+
+		em.remove(updatedOS);
+		OS shouldBeNull = em.find(OS.class, newId);
+		assertNull("Failed to delete", shouldBeNull);
+	}
+
+	private void clearData() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+		System.out.println("Dumping old records...");
+		em.createQuery("delete from com.ericsson.msc.group5.entities.HierInfo").executeUpdate();
+		utx.commit();
+	}
+
+	private void startTransaction() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+	}
+}

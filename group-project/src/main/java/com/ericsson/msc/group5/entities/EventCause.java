2776e8ecd8e73eeecd18fee6fb1a903f60ec88c1
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-02-18 12:53:13 +0000

    eventcause: add arquillian tests

--- group-project/src/main/java/com/ericsson/msc/group5/entities/EventCause.java
+++ group-project/src/main/java/com/ericsson/msc/group5/entities/EventCause.java
@@ -26,4 +26,12 @@ public class EventCause {
 		this.causeCodeAndEventIdCK = causeCodeAndEventIdCK;
 		this.description = description;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
---
+++ group-project/src/test/java/com/ericsson/msc/group5/entities/EventCauseTest.java
@@ -0,0 +1,80 @@
+package com.ericsson.msc.group5.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class EventCauseTest {
+
+	@Deployment
+	public static Archive <?> createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(EventCause.class.getPackage())
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Inject
+	private UserTransaction utx;
+
+	private static String INITIAL_DESCRIPTION = "RRC CONN SETUP-UE BEARERS REJECTED DUE TO ARP ADM REJ AND LICENSES MISSING";
+	private static String UPDATED_DESCRIPTION = "UE CTXT RELEASE-UNKNOWN OR ALREADY ALLOCATED ENB UE S1AP ID";
+
+	@Before
+	public void preparePersistenceTest() throws Exception {
+		clearData();
+		startTransaction();
+	}
+
+	@After
+	public void commitTransaction() throws Exception {
+		utx.commit();
+	}
+
+	@Test
+	public void failureClassTest() throws Exception {
+		EventCauseCK pk = new EventCauseCK(1, 1);
+		EventCause createdEC = new EventCause(pk, INITIAL_DESCRIPTION);
+		em.persist(createdEC);
+
+		EventCause loadedEC = em.find(EventCause.class, pk);
+		assertEquals("Failed to insert", INITIAL_DESCRIPTION, loadedEC.getDescription());
+
+		loadedEC.setDescription(UPDATED_DESCRIPTION);
+		EventCause updatedEC = em.find(EventCause.class, pk);
+
+		assertEquals("Failed to update", UPDATED_DESCRIPTION, updatedEC.getDescription());
+
+		em.remove(updatedEC);
+		EventCause shouldBeNull = em.find(EventCause.class, pk);
+		assertNull("Failed to delete", shouldBeNull);
+	}
+
+	private void clearData() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+		System.out.println("Dumping old records...");
+		em.createQuery("delete from com.ericsson.msc.group5.entities.EventCause").executeUpdate();
+		utx.commit();
+	}
+
+	private void startTransaction() throws Exception {
+		utx.begin();
+		em.joinTransaction();
+	}
+}

7fc030f3a8e0aa47fd2bb83ace951871d71f4c8e
Harry McLaughlin <hamc17@hotmail.com>
2015-02-28 15:02:34 +0000

    Renamed CellValidatiorTest to have one less "i" :)

---
+++ group-project/src/test/java/com/ericsson/msc/group5/dataIOConsistencyChecks/CellIdValidatorTest.java
@@ -0,0 +1,32 @@
+package com.ericsson.msc.group5.dataIOConsistencyChecks;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class CellIdValidatorTest {
+
+	private boolean expectedResult;
+	private Integer cellId;
+
+	@Parameters
+	public static List <Object []> params() {
+		Object [][] data = new Object[][] { {false, 4000}, {true, 3200}};
+		return Arrays.asList(data);
+	}
+
+	public CellIdValidatorTest(boolean expectedResult, Integer cellId) {
+		this.cellId = cellId;
+		this.expectedResult = expectedResult;
+	}
+
+	@Test
+	public void validateCellId() {
+		assertEquals(expectedResult, Validator.validateCellId(cellId));
+	}
+}

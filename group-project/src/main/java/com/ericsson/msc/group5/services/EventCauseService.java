94631103107d8aa612cf73495760aa2702786756
Siobhan <Siobhan@Sue>
2015-03-07 16:09:39 +0000

    EJB for EventCause

---
+++ group-project/src/main/java/com/ericsson/msc/group5/services/EventCauseService.java
@@ -0,0 +1,15 @@
+package com.ericsson.msc.group5.services;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+
+import com.ericsson.msc.group5.entities.EventCause;
+import com.ericsson.msc.group5.entities.EventCauseCK;
+@Local
+public interface EventCauseService {
+
+	public Collection<EventCause> getCauseCode();
+	
+	public void addEventCause(Collection <EventCauseCK> eventCauses);
+}
---
+++ group-project/src/main/java/com/ericsson/msc/group5/services/EventCauseServiceEJB.java
@@ -0,0 +1,38 @@
+package com.ericsson.msc.group5.services;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.ericsson.msc.group5.dao.EventCauseDAO;
+import com.ericsson.msc.group5.entities.EventCause;
+import com.ericsson.msc.group5.entities.EventCauseCK;
+
+
+@Stateless
+@Local
+public class EventCauseServiceEJB implements EventCauseService{
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Inject
+	private EventCauseDAO dao;
+
+	@Override
+	public Collection <EventCause> getCauseCode() {
+		return dao.getAllEventCauses();
+	}
+
+	@Override
+	public void addEventCause(Collection<EventCauseCK> eventCauses) {
+		dao.batchInsertEventCause(eventCauses);
+		
+	}
+	
+
+}

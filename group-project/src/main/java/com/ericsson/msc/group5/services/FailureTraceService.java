0cc62fd9b756c683f95ef97ad03f074cb8f6b025
SiobhanKavanagh <siobhan_kavanagh04@hotmail.com>
2015-03-08 21:26:04 +0000

    Added FailureTraceEJB (still more methods to add)

---
+++ group-project/src/main/java/com/ericsson/msc/group5/services/FailureTraceService.java
@@ -0,0 +1,16 @@
+package com.ericsson.msc.group5.services;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+
+import com.ericsson.msc.group5.entities.FailureTrace;
+
+@Local
+public interface FailureTraceService {
+
+	public Collection <FailureTrace> getFailureTraces();
+
+	public void addFailureTraces(Collection <FailureTrace> failureTraces);
+	
+}
---
+++ group-project/src/main/java/com/ericsson/msc/group5/services/FailureTraceServiceEJB.java
@@ -0,0 +1,33 @@
+package com.ericsson.msc.group5.services;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import com.ericsson.msc.group5.dao.FailureTraceDAO;
+import com.ericsson.msc.group5.entities.FailureTrace;
+
+@Stateless
+@Local
+public class FailureTraceServiceEJB implements FailureTraceService{
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Inject
+	private FailureTraceDAO dao;
+	
+	@Override
+	public Collection<FailureTrace> getFailureTraces() {
+		return dao.getAllFailureTraces();
+	}
+
+	@Override
+	public void addFailureTraces(Collection<FailureTrace> failureTraces) {
+		dao.batchInsertFailureTrace(failureTraces);
+	}
+
+}

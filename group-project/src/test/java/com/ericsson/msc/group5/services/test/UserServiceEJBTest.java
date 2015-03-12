9cd44979553143c4bf3352cfdc36d91ce3cff232
Siobhan <Siobhan@147.252.138.230>
2015-03-12 12:56:23 +0000

    Changed name

---
+++ group-project/src/test/java/com/ericsson/msc/group5/services/test/UserServiceEJBTest.java
@@ -0,0 +1,41 @@
+package com.ericsson.msc.group5.services.test;
+
+import static org.junit.Assert.*;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ericsson.msc.group5.entities.User;
+import com.ericsson.msc.group5.services.UserService;
+
+@RunWith(Arquillian.class)
+public class UserServiceEJBTest {
+	
+	
+	
+	@Deployment
+	public static Archive <?> createDeployment(){
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(UserService.class.getPackage())
+				.addAsResource("test-persistence.xml", "META_INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Inject
+	UserService service;
+	
+	
+	User newUser = new User("SAM", "password", 1);
+	
+	@Test
+	public void testUserIsAdded(){
+		
+		assertEquals(newUser.getUsername(), "SAM");
+	}
+
+}

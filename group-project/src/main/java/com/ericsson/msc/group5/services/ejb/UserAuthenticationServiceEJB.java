b7048b537089947f810bee58432408929325777c
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-03-13 17:56:21 +0000

    jboss & login: link login, change data source name for jboss

--- group-project/src/main/java/com/ericsson/msc/group5/services/ejb/UserAuthenticationServiceEJB.java
+++ group-project/src/main/java/com/ericsson/msc/group5/services/ejb/UserAuthenticationServiceEJB.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import com.ericsson.msc.group5.dao.UserDAO;
 import com.ericsson.msc.group5.entities.User;
 import com.ericsson.msc.group5.services.UserAuthenticationService;
+import com.ericsson.msc.group5.utils.PasswordGenerator;
 
 @Stateless
 @Local
@@ -16,7 +17,9 @@ public class UserAuthenticationServiceEJB implements UserAuthenticationService {
 
 	public String authenticateUser(String username, String password) {
 		User user = userDAO.getUser(username);
-		if (user != null && user.getPassword().equals(password))
+		System.out.println("password is " + password);
+		System.out.println("password is " + PasswordGenerator.generate(password));
+		if (user != null && user.getPassword().equals(PasswordGenerator.generate(password)))
 			return user.getRole();
 		return null;
 	}
--- group-project/src/main/java/com/ericsson/msc/group5/utils/PasswordGenerator.java
+++ group-project/src/main/java/com/ericsson/msc/group5/utils/PasswordGenerator.java
@@ -6,8 +6,8 @@ import org.jboss.security.auth.spi.Util;
  */
 public class PasswordGenerator {
 	public static void main(String[] args) {
-		String password = "tester";
-		System.out.println(new PasswordGenerator().generate(password));
+		String password = "admin";
+		System.out.println(PasswordGenerator.generate(password));
 	}
 
 	public static String generate(String password) {

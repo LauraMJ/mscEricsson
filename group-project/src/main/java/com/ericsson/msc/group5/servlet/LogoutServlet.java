ca9e9e82ab32ded975147783723ca5a13454ccf1
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-03-15 09:30:55 +0000

    index.html: better heading for imsi filtering

--- group-project/src/main/java/com/ericsson/msc/group5/servlet/LogoutServlet.java
+++ group-project/src/main/java/com/ericsson/msc/group5/servlet/LogoutServlet.java
@@ -1,51 +1,37 @@
 package com.ericsson.msc.group5.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class LogoutServlet extends HttpServlet {
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
-
-		HttpSession session = request.getSession(false);
-		if (session == null || session.getAttribute("username") == null) {
-			response.sendRedirect("login.html");
-		}
-		else {
-			response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-			response.setHeader("Pragma", "no-cache");
-			response.setDateHeader("Expires", 0);
-			chain.doFilter(req, res);
-		}
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		// response.setContentType("text/html");
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
 
 		request.getRequestDispatcher("logoutredirect.html").include(request, response);
 
 		request.logout();
-
-		out.print("You are successfully logged out!");
-
-		out.close();
-
-		// response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-		// response.setHeader("Pragma", "no-cache");
-		// response.setDateHeader("Expires", 0);
-		//
-		// request.getSession().invalidate();
-		// response.sendRedirect(request.getContextPath() + "/logout.html");
 	}
+
+	// public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+	// HttpServletRequest request = (HttpServletRequest) req;
+	// HttpServletResponse response = (HttpServletResponse) res;
+	//
+	// HttpSession session = request.getSession(false);
+	// if (session == null || session.getAttribute("username") == null) {
+	// response.sendRedirect("login.html");
+	// }
+	// else {
+	// response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+	// response.setHeader("Pragma", "no-cache");
+	// response.setDateHeader("Expires", 0);
+	// chain.doFilter(req, res);
+	// }
+	// }
 }

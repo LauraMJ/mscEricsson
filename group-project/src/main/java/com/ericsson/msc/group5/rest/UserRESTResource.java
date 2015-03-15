4b39995a5d649c21a576c610676201a3f4b79e4a
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-03-15 09:11:18 +0000

    logout: functionality to log the user out - uses servlets and html window replacement to ensure that the user won't be able to access the cached page by clicking on the back button.

--- group-project/src/main/java/com/ericsson/msc/group5/rest/UserRESTResource.java
+++ group-project/src/main/java/com/ericsson/msc/group5/rest/UserRESTResource.java
@@ -2,17 +2,21 @@ package com.ericsson.msc.group5.rest;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
+import javax.ejb.SessionContext;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import com.ericsson.msc.group5.entities.User;
 import com.ericsson.msc.group5.services.UserService;
 
+@Path("/")
 public class UserRESTResource {
 
 	@EJB
@@ -20,6 +24,8 @@ public class UserRESTResource {
 
 	@Resource
 	private LoginContext loginContext;
+	
+    @Resource SessionContext ctx;
 
 	@POST
 	@Path("/add/user")
@@ -34,16 +40,17 @@ public class UserRESTResource {
 		}
 	}
 
-	@POST
-	@Path("/logout")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response logout() {
-		try {
-			loginContext.logout();
-		}
-		catch (LoginException e) {
-			e.printStackTrace();
-		}
-		return Response.ok().status(200).entity("logout").build();
-	}
+//	@POST
+//	@Path("/logout")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response logout(@Context SecurityContext sc) {
+////		ctx.getContextData().
+//		try {
+//			loginContext.logout();
+//		}
+//		catch (LoginException e) {
+//			e.printStackTrace();
+//		}
+//		return Response.ok().status(200).entity("logout").build();
+//	}
 }

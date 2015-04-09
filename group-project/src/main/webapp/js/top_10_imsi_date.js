32345e3e88afc9b42549aee9540405ddc4ae7351
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-04-09 18:32:00 +0100

    frontend: top 10 failing imsis bootstrap

---
+++ group-project/src/main/webapp/js/top_10_imsi_date.js
@@ -0,0 +1,37 @@
+function getTop10IMSI() {
+	var url = '../rest/query/topTenIMSIsWithFailures';
+
+	var fromDateTime = moment($("#fromDateTime").data("DateTimePicker").date())
+			.format("YYYY-DD-MM HH:mm");
+	var toDateTime = moment($("#toDateTime").data("DateTimePicker").date())
+			.format("YYYY-DD-MM HH:mm");
+
+	var JSONObject = {
+		"DateOne" : fromDateTime,
+		"DateTwo" : toDateTime
+	};
+	JSONObject = JSON.stringify(JSONObject);
+
+	$.ajax({
+		url : url,
+		type : "POST",
+		data : JSONObject,
+		contentType : "application/json",
+		dataType : 'json',
+		success : populateTable
+	});
+}
+
+function populateTable(data) {
+	var t = $('#datatable-1').DataTable();
+	
+	if (data.length == 0) {
+		alert("No results for lookup.");
+	}
+
+	var i;
+	for (i = 0; i < data.length; i++) {
+		t.row.add([ data[i] ]);
+	}
+	t.draw();
+}

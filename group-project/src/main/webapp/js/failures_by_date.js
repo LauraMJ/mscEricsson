696a711244b2402ab41dddaaa95d0806dfbb6582
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-04-12 18:19:22 +0100

    failures for date: fix table not clearing, add validation checks

--- group-project/src/main/webapp/js/failures_by_date.js
+++ group-project/src/main/webapp/js/failures_by_date.js
@@ -6,6 +6,17 @@ function getFailuresByDate() {
 	var toDateTime = moment($("#toDateTime").data("DateTimePicker").date())
 			.format("YYYY-MM-DD HH:mm");
 
+	if (fromDateTime == "Invalid date") {
+		alert("The 'From' date field cannot be empty.");
+		$("#fromDateTime").focus();
+		return;
+	}
+	if (toDateTime == "Invalid date") {
+		alert("The 'To' date field cannot be empty.");
+		$("#toDateTime").focus();
+		return;
+	}
+
 	var JSONObject = {
 		"Date1" : fromDateTime,
 		"Date2" : toDateTime
@@ -24,6 +35,7 @@ function getFailuresByDate() {
 
 function populateTable(data) {
 	var t = $('#datatable-1').DataTable();
+	t.clear();
 
 	var i;
 	for (i = 0; i < data.length; i++) {

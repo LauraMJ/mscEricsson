5c4fbb8ed39c2335bd5490add0886bcc845c921e
SiobhanKavanagh <siobhan_kavanagh04@hotmail.com>
2015-04-13 11:54:05 +0100

    All validation tests passing

--- group-project/src/test/java/com/ericsson/msc/group5/dataIOConsistencyChecks/DateValidatorTest.java
+++ group-project/src/test/java/com/ericsson/msc/group5/dataIOConsistencyChecks/DateValidatorTest.java
@@ -2,38 +2,56 @@ package com.ericsson.msc.group5.dataIOConsistencyChecks;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.ericsson.msc.group5.dao.FailureTraceDAO;
+import com.ericsson.msc.group5.dao.UserDAO;
+import com.ericsson.msc.group5.dao.jpa.JPACountryCodeNetworkCodeDAO;
+import com.ericsson.msc.group5.dao.jpa.JPACountryDAO;
+import com.ericsson.msc.group5.dao.jpa.JPAFailureTraceDAO;
+import com.ericsson.msc.group5.entities.Country;
+import com.ericsson.msc.group5.entities.FailureTrace;
+import com.ericsson.msc.group5.services.DataImportService;
+import com.ericsson.msc.group5.services.ejb.DataImportServiceEJB;
 import com.ericsson.msc.group5.services.ejb.ValidatorServiceEJB;
 
-@RunWith(Parameterized.class)
+@RunWith(Arquillian.class)
 public class DateValidatorTest {
 
+	@Deployment
+	public static Archive <?> createDeployment() {
+		PomEquippedResolveStage pom = Maven.resolver().loadPomFromFile("pom.xml");
+		File [] commonsLang = pom.resolve("org.apache.poi:poi").withTransitivity().asFile();
+		
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(DataImportServiceEJB.class.getPackage())
+				.addPackage(DataImportService.class.getPackage()).addPackage(FailureTraceDAO.class.getPackage()).addPackage(JPAFailureTraceDAO.class.getPackage()).addPackage(FailureTrace.class.getPackage())
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsLibraries(commonsLang)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
 	private boolean expectedResult;
 	private String dateString;
 	
-	@EJB
+	@Inject
 	ValidatorServiceEJB service;
-
-	@Parameters
-	public static List <Object []> params() {
-		Object [][] data = new Object[][] { {true, "1/1/11 15:35"}, {true, "31/1/12 00:00"}, {true, "29/02/12 00:00"}, {false, "29/02/15"},
-				{false, "31/04/15"}, {false, "00/02/15"}, {false, "19/02/75"}};
-		return Arrays.asList(data);
-	}
-
-	public DateValidatorTest(boolean expectedResult, String dateString) {
-		this.dateString = dateString;
-		this.expectedResult = expectedResult;
-	}
 
 	@Test
 	public void checkIfValidDate() {

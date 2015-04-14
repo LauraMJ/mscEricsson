aa27f7eafc5cc10d1c5ba214cb1714a4c2ad82ff
Conor Clarke <con.clarke3@gmail.com>
2015-04-14 11:05:46 +0100

    UserAutheuticationServiceEJB test updated

--- group-project/src/test/java/com/ericsson/msc/group5/services/ejb/test/UserAuthenicationServiceEJBTest.java
+++ group-project/src/test/java/com/ericsson/msc/group5/services/ejb/test/UserAuthenicationServiceEJBTest.java
@@ -1,9 +1,7 @@
 package com.ericsson.msc.group5.services.ejb.test;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import javax.ejb.EJB;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -11,7 +9,6 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import com.ericsson.msc.group5.dao.UserDAO;
 import com.ericsson.msc.group5.dao.jpa.JPAUserDAO;
 import com.ericsson.msc.group5.entities.User;
@@ -22,23 +19,27 @@ import com.ericsson.msc.group5.services.ejb.UserServiceEJB;
 
 @RunWith(Arquillian.class)
 public class UserAuthenicationServiceEJBTest {
-	
+
 	@Deployment
 	public static JavaArchive createDeployment() {
-		return ShrinkWrap.create(JavaArchive.class, "test.jar").addPackage(User.class.getPackage())
-				.addClasses(User.class, UserAuthenticationServiceEJB.class, UserAuthenticationService.class, UserServiceEJB.class, UserService.class, UserDAO.class, JPAUserDAO.class)
-				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-	}
-	
-	@EJB
-	UserAuthenticationService service;
-	
-	@Test
-	public void authenticateUserTest(){
-		User user = new User("John", "nothing", "Customer");
-		
-		//service.authenticateUser("John", "nothing");
-		assertEquals(user.getRole(), "Customer");
+		return ShrinkWrap
+				.create(JavaArchive.class, "test.jar")
+				.addPackage(User.class.getPackage())
+				.addClasses(User.class, UserAuthenticationServiceEJB.class, UserAuthenticationService.class, UserServiceEJB.class, UserService.class,
+						UserDAO.class, JPAUserDAO.class).addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 
+	@EJB
+	UserAuthenticationService service;
+
+	@EJB
+	UserService userService;
+
+	@Test
+	public void authenticateUserTest() {
+		userService.addUser("John", "nothing", "Customer");
+		User user = userService.getUser("John");
+		assertEquals(service.authenticateUser(user.getUsername(), user.getPassword()), "Customer");
+	}
 }

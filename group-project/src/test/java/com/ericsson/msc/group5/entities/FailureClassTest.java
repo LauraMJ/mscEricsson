017b1de170b39e099c5205b42880253f30c3eb02
Szymon Zielinski <szymon.zielinski14@gmail.com>
2015-04-15 00:09:41 +0100

    failureclastest: use ejb and transactional rollbacks

--- group-project/src/test/java/com/ericsson/msc/group5/entities/FailureClassTest.java
+++ group-project/src/test/java/com/ericsson/msc/group5/entities/FailureClassTest.java
@@ -4,62 +4,59 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Collection;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.ericsson.msc.group5.services.FailureClassService;
 
 @RunWith(Arquillian.class)
+@Transactional
 public class FailureClassTest {
 
 	@PersistenceContext
 	private EntityManager em;
 
-	@Inject
-	private UserTransaction utx;
-
 	private static String INITIAL_DESCRIPTION = "HIGH PRIORITY ACCESS";
-	private static String UPDATED_DESCRIPTION = "EMERGENCY";
-
-	@Before
-	public void preparePersistenceTest() throws Exception {
-		clearData();
-		startTransaction();
-	}
-
-	@After
-	public void commitTransaction() throws Exception {
-		utx.commit();
-	}
+	
+	@Inject
+	private FailureClassService failureClassService;
 
 	@Test
+	@Transactional(TransactionMode.ROLLBACK)
 	public void basicCRUDTest() throws Exception {
 		int id = 0;
-		int newId = 50;
 
 		FailureClass createdFC = new FailureClass(id, INITIAL_DESCRIPTION);
-		em.persist(createdFC);
+		failureClassService.addFailureClass(createdFC);
 
-		FailureClass loadedFC = em.find(FailureClass.class, id);
+		ArrayList <FailureClass> failureClasses = new ArrayList <>();
+		Collection <?> failureClassesCollection = failureClassService.getFailureClasses();
+
+		for (Object e : failureClassesCollection) {
+			failureClasses.add((FailureClass) e);
+		};
+		FailureClass loadedFC = null;
+		for (FailureClass f : failureClasses) {
+			if (f.getFailureClass().equals(id)) {
+				loadedFC = f;
+				break;
+			}
+		}
 		assertEquals("Failed to insert", INITIAL_DESCRIPTION, loadedFC.getDescription());
-
-		loadedFC.setDescription(UPDATED_DESCRIPTION);
-		loadedFC.setFailureClass(newId);
-		FailureClass updatedFC = em.find(FailureClass.class, id);
-		assertEquals("Failed to update", UPDATED_DESCRIPTION, updatedFC.getDescription());
-		assertEquals("Failed to update", newId, (int) updatedFC.getFailureClass());
-
-		em.remove(updatedFC);
-		FailureClass shouldBeNull = em.find(FailureClass.class, id);
-		assertNull("Failed to delete", shouldBeNull);
 	}
 	
 	@Test
+	@Transactional(TransactionMode.ROLLBACK)
 	public void testEquality(){
 		FailureClass failureClass = new FailureClass(1, "description");
 		FailureClass otherFailureClass = new FailureClass(2, "other description");
@@ -71,18 +68,5 @@ public class FailureClassTest {
 		assertFalse((new FailureClass()).equals(failureClass));
 		assertFalse(failureClass.equals(otherFailureClass));
 		assertTrue(failureClass.hashCode() == (new FailureClass(1, "other failure class").hashCode()));
-	}
-
-	private void clearData() throws Exception {
-		utx.begin();
-		em.joinTransaction();
-		System.out.println("Dumping old records...");
-		em.createQuery("delete from com.ericsson.msc.group5.entities.FailureClass").executeUpdate();
-		utx.commit();
-	}
-
-	private void startTransaction() throws Exception {
-		utx.begin();
-		em.joinTransaction();
 	}
 }

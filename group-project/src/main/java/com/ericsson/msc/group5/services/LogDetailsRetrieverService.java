4110cb6c9ceee29015dde88bf82f682f6b6f97fd
Conor Clarke <con.clarke3@gmail.com>
2015-04-14 12:56:45 +0100

    LogDetailsRetriever now an EJB

--- group-project/src/main/java/com/ericsson/msc/group5/services/LogDetailsRetrieverService.java
+++ group-project/src/main/java/com/ericsson/msc/group5/services/LogDetailsRetrieverService.java
@@ -1,8 +1,13 @@
 package com.ericsson.msc.group5.services;
 
+import java.util.ArrayList;
+import javax.ejb.Local;
 import org.json.simple.JSONObject;
 
+@Local
 public interface LogDetailsRetrieverService {
 
 	public JSONObject retrieveLogDetailsAsJson();
+
+	JSONObject createJsonObjectFromArrayListOfDetails(ArrayList <String> listOfDetailsForLastLogEntry);
 }

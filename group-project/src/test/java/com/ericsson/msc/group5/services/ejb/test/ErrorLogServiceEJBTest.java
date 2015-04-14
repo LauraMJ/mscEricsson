6fc3cb2c5ddacbc93f5f3b8ea919f737fe4335cb
SiobhanKavanagh <siobhan_kavanagh04@hotmail.com>
2015-04-14 18:11:51 +0100

    ErrorLogServiceEJB Test passing

--- group-project/src/test/java/com/ericsson/msc/group5/services/ejb/test/ErrorLogServiceEJBTest.java
+++ group-project/src/test/java/com/ericsson/msc/group5/services/ejb/test/ErrorLogServiceEJBTest.java
@@ -1,13 +1,25 @@
 package com.ericsson.msc.group5.services.ejb.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.ejb.EJB;
 import javax.inject.Inject;
+import javax.transaction.UserTransaction;
+
 import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import com.ericsson.msc.group5.dao.ErrorLogDAO;
+import com.ericsson.msc.group5.entities.ErrorLog;
 import com.ericsson.msc.group5.services.ErrorLogWriterService;
 
 @RunWith(Arquillian.class)
@@ -21,12 +33,25 @@ public class ErrorLogServiceEJBTest {
 	@Test
 	public void writeToErrorLogTest() {
 
-		HSSFRow rowOfBaseData = null;
-		String errorDescription = "errors";
+		HSSFWorkbook workbook = null;
 
-		service.writeToErrorLog(rowOfBaseData, errorDescription);
-
-		assertTrue("An object failed to be retrieved", errorLogDAO.getAllErrorLogs().contains(errorDescription));
-
+		try {
+			POIFSFileSystem fileSystem = new POIFSFileSystem(this.getClass().getResourceAsStream("/TestingDataset.xls"));
+			workbook = new HSSFWorkbook(fileSystem);
+		}
+		catch (IOException e) {}
+		HSSFSheet worksheet = (HSSFSheet) workbook.getSheetAt(0);
+		HSSFRow row = (HSSFRow) worksheet.getRow(1);
+		
+	
+		Calendar currentDate = Calendar.getInstance(); 
+		SimpleDateFormat formatter= new SimpleDateFormat("dd/mm/yyyy HH:mm:ss"); 
+		String dateNow = formatter.format(currentDate.getTime());
+		
+		ErrorLog errorlog = new ErrorLog(dateNow, "error", "data");
+		
+		service.writeToErrorLog(row, "error");
+		assertEquals("error", errorlog.getErrorDescription());
 	}
+	
 }

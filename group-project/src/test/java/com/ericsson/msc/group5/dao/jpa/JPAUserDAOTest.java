ebab51592465a26730dbe151bb9222c19d6d80c4
Harry McLaughlin <hamc17@hotmail.com>
2015-04-15 09:34:12 +0100

    Changed the JPAUser test to transactional.

--- group-project/src/test/java/com/ericsson/msc/group5/dao/jpa/JPAUserDAOTest.java
+++ group-project/src/test/java/com/ericsson/msc/group5/dao/jpa/JPAUserDAOTest.java
@@ -5,60 +5,37 @@ import static org.junit.Assert.assertTrue;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.UserTransaction;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.After;
-import org.junit.Before;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import com.ericsson.msc.group5.dao.UserDAO;
-import com.ericsson.msc.group5.entities.User;
+import com.ericsson.msc.group5.services.UserService;
 
 @RunWith(Arquillian.class)
+@Transactional
 public class JPAUserDAOTest {
 
 	@PersistenceContext
 	private EntityManager em;
 
 	@Inject
-	private UserDAO dao;
-
-	@Inject
-	private UserTransaction utx;
-
-	@Before
-	public void preparePersistenceTest() throws Exception {
-		clearData();
-		startTransaction();
-	}
-
-	@After
-	public void commitTransaction() throws Exception {
-		utx.commit();
-	}
+	private UserService userService;
 
 	@Test
+	@Transactional(TransactionMode.ROLLBACK)
 	public void addUserTest() {
-		User user = new User("user", "password", "administrator");
-		dao.addUser(user);
-		assertNotNull(dao.getUser("user"));
+		// User user = new User("user", "password", "administrator");
+		userService.addUser("user", "password", "administrator");
+		assertNotNull(userService.getUser("user"));
 	}
 
 	@Test
+	@Transactional(TransactionMode.ROLLBACK)
 	public void getUserTest() {
-		assertNotNull(dao.getUser("user"));
-		assertTrue(dao.getUser("user").getRole() == "administrator");
+		userService.addUser("user", "password", "administrator");
+		assertNotNull(userService.getUser("user"));
+		assertTrue(userService.getUser("user").getRole() == "administrator");
 	}
 
-	private void clearData() throws Exception {
-		utx.begin();
-		em.joinTransaction();
-		em.createQuery("delete from com.ericsson.msc.group5.entities.FailureTrace").executeUpdate();
-		utx.commit();
-	}
-
-	private void startTransaction() throws Exception {
-		utx.begin();
-		em.joinTransaction();
-	}
 }
